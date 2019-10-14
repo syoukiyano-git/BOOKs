6eab3a5292c60e21ecf3c1ba021c43a3681b8be9
Syouki Yano <syouki0728@i.softbank.jp>
2019-10-14 22:20:44 +0000

    fix seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 
-products_csv = CSV.readlines("db/product.csv")
+products_csv = CSV.readlines("db/products.csv")
 products_csv.each do |row|
     Product.create(title: row[1], image_url: row[2], author: row[5])
 end 
