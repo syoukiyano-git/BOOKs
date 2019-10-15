16615524d40a982a6ba1624dd1922b334e1aa294
Syouki Yano <syouki0728@i.softbank.jp>
2019-10-15 06:42:34 +0000

    add routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do 
     root :to => "devise/sessions#new"
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end 
   resources :users, only: :show
   resources :rankings, only: :index
